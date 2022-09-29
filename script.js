455623b037c37d88dfdcd86ae0db10486a0922ec
guillesaya <guillermo.sayavedra@gmail.com>
2022-09-29 19:53:44 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function envio(){
+   codigoEnvio = prompt("Cual es su código postal: ")
+   if (codigoEnvio != 2900) {
+    alert("El envio sale $1000")
+    
+   }
+   else{
+    alert("El envio es gratis")
+   }
+} 
+
+function deseaSalir() {
+   alert("Esta saliendo del pagina.")
+}
+  
